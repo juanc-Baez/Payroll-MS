b84e1d7f5a912ed3d19a50295dfc7b343f2ad3cc
juanc-Baez <juanbaez8c@gmail.com>
2024-10-22 18:53:01 -0300

    Project created

---
+++ src/main/java/com/juanapi/payrollms/PayrollMsApplication.java
@@ -0,0 +1,13 @@
+package com.juanapi.payrollms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanapi/payrollms/PayrollMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanapi.payrollms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
